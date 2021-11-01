88c931945e21a53e3e3c8c0b8f8f83e1ae9deb1b
Goutham S <gouthamshine@gmail.com>
2021-11-01 14:21:38 +0530

    config update | external tomcat

--- src/main/java/in/net/goutham/greeting/GreetingApplication.java
+++ src/main/java/in/net/goutham/greeting/GreetingApplication.java
@@ -2,12 +2,19 @@ package in.net.goutham.greeting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GreetingApplication {
+public class GreetingApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GreetingApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(GreetingApplication.class);
 	}
 
 }
